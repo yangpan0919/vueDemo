ebbddc92e9d882103d8d78e6231cc7f591c8aac6
杨攀 <473075889>
2019-11-07 14:04:33 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@
  */
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
-// 创建vm
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: {App}, // 映射组件标签
-  template: '<App/>' // 指定需要渲染到页面的模板
+  template: '<App/>', // 指定需要渲染到页面的模板
+  router  // 注册路由器
 })
